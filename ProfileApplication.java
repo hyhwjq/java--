78e6b3cc5722bd1ce60e4c65b82c7099a0b56b25
hyhwjq <34830452+hyhwjq@users.noreply.github.com>
2018-03-05 22:08:57 +0800

    设置profile

--- ProfileApplication.java
+++ ProfileApplication.java
@@ -9,6 +9,8 @@ public class ProfileApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(ApplicationContext.class, args);
+		//setActiveProfiles可设置
+		//context.getEnvironment().setActiveProfiles("screen");
 		String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
 		for (String profile : activeProfiles) {  
 	        System.out.println("Spring Boot 使用profile为:{}"+profile);  
